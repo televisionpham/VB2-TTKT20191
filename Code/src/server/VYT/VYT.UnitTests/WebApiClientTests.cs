b22f5f18bdba24e5e5fd67f9875f928ac510cef8
Van Pham <television_pham@yahoo.com>
2020-09-21 23:20:29 +0700

    WIP: writing web api client

---
+++ Code/src/server/VYT/VYT.UnitTests/WebApiClientTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VYT.ApplicationServiceClient;
+
+namespace VYT.UnitTests
+{
+    [TestClass]
+    public class WebApiClientTests
+    {
+        [TestMethod]
+        public void Can_get_job()
+        {
+            var client = new WebApiClient("http://localhost/VYT.ApplicationService");
+            var result = client.GetJob(9);
+            result.Wait();
+            Assert.IsNotNull(result.Result);
+            Assert.AreEqual(9, result.Result.Id);
+        }
+    }
+}
